3a35690a8e7dd44da0e9a7b3fc79cc40a009fa22
SawyerZhu <sawyer.zhu@me.com>
2017-04-02 20:30:09 +0800

    no message

--- Library/Android.mk
+++ Library/Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-CCX_ROOT := $(LOCAL_PATH)/../../..
-CCX_ROOT_2 := $(LOCAL_PATH)/../../cocos2d-x
+CCX_ROOT := $(LOCAL_PATH)/../../sakura_client/sakura/cocos2d
 GAF_LIB_SOURCES := $(LOCAL_PATH)/Sources
 
 LOCAL_MODULE := gafplayer_static
@@ -12,14 +11,14 @@ LOCAL_MODULE_FILENAME := libgafplayer
 
 CLASSES_FILES := $(wildcard $(GAF_LIB_SOURCES)/*.cpp)
 LOCAL_SRC_FILES := $(CLASSES_FILES:$(LOCAL_PATH)/%=%)
+$(warning $(LOCAL_SRC_FILES))
 
 LOCAL_C_INCLUDES := \
 $(CCX_ROOT)/cocos \
 $(CCX_ROOT)/cocos/platform/android \
 $(CCX_ROOT)/external \
-$(CCX_ROOT_2)/cocos \
-$(CCX_ROOT_2)/cocos/platform/android \
-$(CCX_ROOT_2)/external \
-$(GAF_LIB_SOURCES)
+$(GAF_LIB_SOURCES) \
+
+LOCAL_EXPORT_C_INCLUDES := $(GAF_LIB_SOURCES)
 
 include $(BUILD_STATIC_LIBRARY)
